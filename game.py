1eb13442eb3111d6d9aa47ee9f5378b812fb39e0
Paul Campbell <apcampbell12@gmail.com>
2022-08-07 12:59:20 -0500

    Add method for player to choose 4 colors

--- game.py
+++ game.py
@@ -16,8 +16,30 @@ class Game:
             arr.append(choice)
         self.code = arr
 
+    def make_choice(self):
+        print("Choose 4 of these colors with no repeats\n")
+        for i in self.colors:
+            print(f"{i}", end=" ")
+        print("\n")
+        arr = []
+        while len(arr) < 4:
+            choice = input(f"Color #{len(arr)+1}\n")
+            if choice in arr:
+                print("That color has already been chosen")
+                continue
+            elif (choice == 'quit'):
+                return
+            elif (choice not in self.colors):
+                print("That's not a color option. Please try again")
+                continue
+            else:
+                arr.append(choice)
+        return arr     
+
     
 
 game = Game()
 game.make_code()
 print(game.code)
+print(game.make_choice())
+

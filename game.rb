c63bf99417984d7442dc0d81a51cbfb69804ede7
Paul Campbell <apcampbell12@gmail.com>
2022-08-07 16:37:25 -0500

    Add method for choosing your guess

--- game.rb
+++ game.rb
@@ -5,12 +5,40 @@ class Game
         @colors = ['blue', 'red', 'green', 'yellow',
             'black', 'orange', 'white', 'purple']
         @code = []
+        @player_choice = []
     end
 
     def make_code()
         arr = @colors.sample(4)
         @code = arr
     end
+
+    def make_choice()
+        puts "Choose 4 of these colors with no repeats"
+        puts
+        @colors.each do |i|
+            print i + " "
+        end
+        puts
+        arr = []
+        while arr.length < 4
+            puts "Color ##{arr.length + 1}"
+            choice = gets.chomp
+            if arr.include?(choice)
+                print("That color has already been chosen")
+                next
+            elif (choice == 'quit')
+                return
+            elif (!@colors.include?(choice))
+                print("That's not a color option. Please try again")
+                next
+            else
+                arr << choice
+            end
+        end
+        @player_choice = arr
+    end
 end
 
 game = Game.new()
+game.make_choice
